6420d9c57e0137982f1e4b384d2b1d12e6296162
Oluwatobiloba <alomajatobi2001@gmail.com>
2025-05-12 07:38:12 +0100

    Initail commit

---
+++ trivia.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "trivia bot";
+let botLocation = "London";
+let favoriteLanguage = "JavaScipt";
+
+console.log("My name is " + botName + " and I live on "+ botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact =favoriteLanguage + " is one of the most important language in web development.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used in the development of both frontend and backend.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " has many frameworks you can use to enhance productivity, both for frontend and backend.";
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation+ ".") ;
